8155b086fda47ece9a5830059016a104754723f8
frdhcv <faridhv263@gmail.com>
2025-04-02 22:43:03 +0400

    Final!!

--- src/main/java/com/example/portfolio/PortfolioApplication.java
+++ src/main/java/com/example/portfolio/PortfolioApplication.java
@@ -1,12 +1,18 @@
 package com.example.portfolio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 public class PortfolioApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
     }
 }
-
